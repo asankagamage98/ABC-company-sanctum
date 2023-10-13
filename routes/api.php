23889494306bda08e8ccbebf2fcda741872871a4
asankagamage98 <asankagamage1998@gmail.com>
2023-10-13 15:32:42 +0530

    created public routes register,login,getall & protect routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\http\Controllers\ProductController;
+use App\http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,14 +16,22 @@ use App\http\Controllers\ProductController;
 |
 */
 
-//test api
+//public routes  
 Route::get('/products',[ProductController::class,'index']);
-Route::post('/products',[ProductController::class,'store']);
 Route::get('products/{id}',[ProductController::class,'show']);
-Route::post('products/{id}',[ProductController::class,'update']);
-Route::delete('products/{id}',[ProductController::class,'destroy']);
-Route::get('products/search/{name}',[ProductController::class,'search']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//new public routes
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
+
+
+//protected routes
+Route::group(['middleware' => ['auth:sanctum']],function(){
+    Route::get('products/search/{name}',[ProductController::class,'search']);
+    Route::post('/products',[ProductController::class,'store']);
+    Route::put('products/{id}',[ProductController::class,'update']);
+    Route::delete('products/{id}',[ProductController::class,'destroy']);
+
+    Route::post('/logout',[AuthController::class,'logout']);
+
 });
